55a93e7e406d78ed73a0425db225dfd1f3fa19e1
Octavio <octaperi@gmail.com>
2017-10-25 20:26:54 -0300

    Add parameter alumnos for detalle-asistencia-dia constructor

--- src/pages/detalle-asistencia-dia/detalle-asistencia-dia.ts
+++ src/pages/detalle-asistencia-dia/detalle-asistencia-dia.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
 
+import { Alumno } from '../../app/model/alumno';
 
 @Component({
   templateUrl: 'detalle-asistencia-dia.html'
 })
 export class DetalleAsistenciaDiaPage { 
 
-
+  alumnos : Array<Alumno>;
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams) {
+    this.alumnos = navParams.get('alumnos');
+  }
 }
